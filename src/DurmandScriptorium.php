8404ccd01be7c7ce581ca7eec449542a4f6a57a0
Etienne Lamoureux <etienne.lamoureux@crystalgorithm.com>
2014-09-22 01:14:26 -0400

    Added listings api consumption

--- src/DurmandScriptorium.php
+++ src/DurmandScriptorium.php
@@ -13,35 +13,72 @@ use EtienneLamoureux\DurmandScriptorium\v2\CollectionApiRequestFactory;
 class DurmandScriptorium
 {
 
+    public static $quaggansEndpointUrl = '/v2/quaggans';
+    public static $listingsEndpointUrl = '/v2/commerce/listings';
     protected $quaggans;
+    protected $listings;
 
     public function __construct()
     {
-	$this->quaggans = new CollectionApiConsumer(new CollectionApiRequestFactory('/v2/quaggans'));
+	$quaggansRequestFactory = new CollectionApiRequestFactory(DurmandScriptorium::$quaggansEndpointUrl);
+	$this->quaggans = new CollectionApiConsumer($quaggansRequestFactory);
+
+	$listingsRequestFactory = new CollectionApiRequestFactory(DurmandScriptorium::$listingsEndpointUrl);
+	$this->listings = new CollectionApiConsumer($listingsRequestFactory);
     }
 
     public function getAllQuaggans($expanded = false)
     {
+	return $this->getAllElements($this->quaggans);
+    }
+
+    public function getQuaggan($id)
+    {
+	return $this->getElement($this->quaggans, $id);
+    }
+
+    public function getQuaggans($ids)
+    {
+	return $this->getElements($this->quaggans, $ids);
+    }
+
+    public function getAllListings($expanded = false)
+    {
+	return $this->getAllElements($this->listings);
+    }
+
+    public function getListing($id)
+    {
+	return $this->getElement($this->listings, $id);
+    }
+
+    public function getListings($ids)
+    {
+	return $this->getElements($this->listings, $ids);
+    }
+
+    private function getAllElements($apiConsumer, $expanded = false)
+    {
 	if ($expanded)
 	{
-	    $data = $this->quaggans->getAllExpanded();
+	    $data = $apiConsumer->getAllExpanded();
 	}
 	else
 	{
-	    $data = $this->quaggans->getAll();
+	    $data = $apiConsumer->getAll();
 	}
 
 	return $data;
     }
 
-    public function getQuaggan($id)
+    private function getElement($apiConsumer, $id)
     {
-	return $this->quaggans->get($id);
+	return $apiConsumer->get($id);
     }
 
-    public function getQuaggans($ids)
+    private function getElements($apiConsumer, $ids)
     {
-	return $this->quaggans->getSome($ids);
+	return $apiConsumer->getSome($ids);
     }
 
 }
