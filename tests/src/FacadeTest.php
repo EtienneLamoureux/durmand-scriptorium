c1f0384d37226d5ad8895e00a6d5b43964415708
Etienne Lamoureux <etienne.lamoureux@crystalgorithm.com>
2014-09-28 10:30:53 -0400

    Fine tuned tools for unit testing

---
+++ tests/src/FacadeTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Crystalgorithm\DurmandScriptorium;
+
+class FacadeTest extends \PHPUnit_Framework_TestCase
+{
+
+    const VALID_AMOUNT_TO_CONVERT = 10000;
+
+    /**
+     * @var Facade
+     */
+    protected $api;
+
+    protected function setUp()
+    {
+	$this->api = new Facade;
+    }
+
+    protected function tearDown()
+    {
+
+    }
+
+    public function testCanReachQuaggans()
+    {
+	$data = $this->api->quaggans()->getAll();
+
+	$this->assertNotNull($data);
+    }
+
+    public function testCanReachListings()
+    {
+	$data = $this->api->listings()->getAll();
+
+	$this->assertNotNull($data);
+    }
+
+    public function testCanReachPrices()
+    {
+	$data = $this->api->prices()->getAll();
+
+	$this->assertNotNull($data);
+    }
+
+    public function testCanReachItems()
+    {
+	$data = $this->api->items()->getAll();
+
+	$this->assertNotNull($data);
+    }
+
+    public function testCanReachCoins()
+    {
+	$data = $this->api->coins()->convert(self::VALID_AMOUNT_TO_CONVERT);
+
+	$this->assertNotNull($data);
+    }
+
+    public function testCanReachGems()
+    {
+	$data = $this->api->gems()->convert(self::VALID_AMOUNT_TO_CONVERT);
+
+	$this->assertNotNull($data);
+    }
+
+}
